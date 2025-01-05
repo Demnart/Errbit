1dee4a13507b7eeab6dc0194cd05d8ea79a2817e
MeusLudus <insidia93@gmail.com>
2025-01-05 22:07:35 +0000

    Added a new line which include user.rb file

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,4 +5,6 @@
 # Copyright:: 2025, The Authors, All Rights Reserved.
 
 include_recipe 'errbit::mongo'
+include_recipe 'errbit::user'
 include_recipe 'errbit::install'
+include_recipe 'errbit::unit'
