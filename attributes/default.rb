69d4c841b7ef38d7a250f6559003dc16fc7de5fb
MeusLudus <insidia93@gmail.com>
2025-01-06 15:57:57 +0000

    Added new attributes for errbit and rvm

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,18 @@ default['user']['path'] = "/home/#{node['user']['name']}"
 default['user']['password'] = '$6$xieb2fuF$NHsnWjvf3RhwpA9Ld/ARZbtAy3ImUzaJWM1WRmvPcaRuNzJjf6Qdb7.Z5JsFLvMTyp9ESN60gT9w6XY.hcc4A1'
 default['user']['shell'] = '/bin/bash'
 
+default['errbit']['repo'] = 'https://github.com/errbit/errbit.git'
+default['errbit']['destination'] = "#{node['user']['path']}/errbit"
+default['errbit']['admin']['name'] = 'admin'
+default['errbit']['admin']['password'] = 'adminpassword'
+default['errbit']['admin']['email'] = 'your_email@example.com'
+default['errbit']['default']['port'] = '3000'
+
+default['rvm']['bin']="#{node['user']['path']}/.rvm/bin/rvm"
+default['rvm']['openssl']['path'] = "#{node['user']['path']}/.rvm/usr/bin/openssl"
+default['rvm']['wrapper']['gem'] = "#{node['user']['path']}/.rvm/wrappers/ruby-2.7.6/gem"
+default['rvm']['wrapper']['bundle'] = "#{node['user']['path']}/.rvm/wrappers/ruby-2.7.6/bundle"
+
 
 
 if platform_family?('debian')
