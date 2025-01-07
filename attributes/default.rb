77f304d537a65db93c6a1ef70f2ff3885e596e00
MeusLudus <insidia93@gmail.com>
2025-01-07 11:02:32 +0000

    Changed uid&gid because in real server uid&gid 1000 can be busy

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['mongodb']['package_version'] = '8.0.0'
 default['user']['name'] = 'meus'
-default['user']['uid'] = '1000'
-default['user']['gid'] = '1000'
+default['user']['uid'] = '1005'
+default['user']['gid'] = '1005'
 default['user']['path'] = "/home/#{node['user']['name']}"
 default['user']['password'] = '$6$xieb2fuF$NHsnWjvf3RhwpA9Ld/ARZbtAy3ImUzaJWM1WRmvPcaRuNzJjf6Qdb7.Z5JsFLvMTyp9ESN60gT9w6XY.hcc4A1'
 default['user']['shell'] = '/bin/bash'
